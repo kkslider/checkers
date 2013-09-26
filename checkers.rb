175fc78de2da46a02b85fb94efd74343507a4fc5
Benjamin Lee <benjamsandwich@gmail.com>
2013-09-26 09:39:30 -0700

    Make class Board

---
+++ checkers.rb
@@ -0,0 +1,19 @@
+class Board
+  attr_accessor :state
+  
+  def initialize
+    
+  end
+  
+  def setup
+    state = Array.new(8) { Array.new(8) }
+  end
+  
+  def add_pieces
+    
+  end
+  
+  
+  
+  
+end
